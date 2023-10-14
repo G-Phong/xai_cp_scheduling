d7e51b5eabbfb5710cdfd4b5d89e869b33548121
ge79kis <ge79kis@mytum.de>
2023-10-14 23:48:38 +0200

    Integrate npm installation into deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+# Installieren von Node.js und npm
+wget -qO- https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
+
+# Ihr restlicher Build-Prozess
 cd xai_frontend
 npm install
 npm run build
